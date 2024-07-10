d874e88f8bf9052f8b7c12b403434eddc14c06f5
jcmoralep <152304974+jcmoralep@users.noreply.github.com>
2024-07-10 16:50:02 -0500

    Se ajustan archivos de configuración para docker

--- Inventory.Api/Program.cs
+++ Inventory.Api/Program.cs
@@ -3,6 +3,10 @@ using Inventory.Api.Middlewares;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(80); // Escuchar en el puerto 80
+});
 builder.Services.InstallServicesInAssembly(builder.Configuration);
 var logger = new LoggerConfiguration()
               .ReadFrom.Configuration(builder.Configuration)
@@ -15,7 +19,6 @@ var app = builder.Build();
 app.UseMiddleware<ErrorHandler>();
 app.UseSwagger();
 app.UseSwaggerUI();
-app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
